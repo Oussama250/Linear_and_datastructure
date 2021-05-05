ef6801cba2e9689943a845e621cd55e48c242375
Oussama Beddiaf <oussama-mias@live.fr>
2021-05-05 18:33:41 +0100

    an algorithme for each case commit.

--- Linear_and_datastructure.js
+++ Linear_and_datastructure.js
@@ -1,22 +1,59 @@
+// find the sum of all distinct elements from the set.
 Algorithm sum
-
 VAR
-charactere : STRING[];
-nbrCharactere : FLOAT := 0;
-nbrMots : FLOAT := 0;
-nbrVowels : FLOAT := 0;
+arr1 : ARRAY_OF INTEGER[2];
+arr2 : ARRAY_OF INTEGER[2];
+a : INTEGER:=0;
+b : INTEGER:=0;
+Sum : FLOAT := 0;
 BEGIN
-WHILE (charactere <> ".") DO
-    charactere := read("Entrer un charactere : ");
-    if (charactere <> "" And charactere.length = 1) THEN 
-        nbrCharactere:=nbrCharactere+1;
-    END_IF
-    if (charactere = " ") THEN
-        nbrMots:=nbrMots+1;
-    END_IF
-    if (charactere = "a" or charactere = "e" or charactere = "i" or charactere = "o" or charactere = "u") THEN
-    nbrVowels:= nbrVowels + 1;
-    END_IF
-END_WHILE
-Write("Le nombre de charactere est :" + nbrCharactere + ", et le nombre de mots est "+nbrMots+1 + ", et le nombre de voyelles est "+nbrVowels);
+FOR a FROM 0 TO arr1.length STEP 1 DO
+    FOR b FROM 0 TO arr2.length STEP 1 DO
+        IF (b=arr2.length-1) THEN
+            Sum:=Sum + arr1[a];
+        END_IF
+    END_FOR 
+END_FOR
+
+FOR a FROM 0 TO arr2.length STEP 1 DO
+    FOR b FROM 0 TO arr1.length STEP 1 DO
+        IF (b=arr1.length-1) THEN
+            Sum:=Sum + arr2[a];
+        END_IF
+    END_FOR 
+END_FOR
+
+Return Sum;
+
+END
+//print the sum of overlapping elements in two sets.
+Algorithm sum2
+VAR
+arr1 : ARRAY_OF INTEGER[2];
+arr2 : ARRAY_OF INTEGER[2];
+arr3 : ARRAY_OF INTEGER[2];
+a : INTEGER:=0;
+b : INTEGER:=0;
+Sum : FLOAT := 0;
+BEGIN
+FOR a FROM 0 TO arr1.length STEP 1 DO
+    FOR b FROM 0 TO arr2.length STEP 1 DO
+        IF (arr1[a]=arr2[b]) THEN
+            arr3[arr3.length]:=arr1[a];
+        END_IF
+    END_FOR 
+END_FOR
+
+FOR a FROM 0 TO arr2.length STEP 1 DO
+    FOR b FROM 0 TO arr1.length STEP 1 DO
+        IF (arr2[a]=arr1[b] And arr3.indexOf(arr2[a]) THEN
+            arr3[arr3.length]:=arr2[a];
+        END_IF
+    END_FOR 
+END_FOR
+
+FOR a FROM 0 TO arr3.length STEP 1 DO
+    Sum := Sum + arr3[a];
+END_FOR
+Return Sum;
 END
